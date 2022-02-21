1bbfcee39eb4b0de9056ed3053ff5d3b883c5d36
numbercutter <jamlets@protonmail.com>
2022-02-20 19:55:35 -0500

    slices

--- generators/sl.py
+++ generators/sl.py
@@ -3,8 +3,6 @@ import glob
 
 sounds = glob.glob("./audio/*")
 sound = AudioSegment.from_file(sounds[0], format="wav")
-slices = sound[::50000]
-print(slices)
 
 
 for i, chunk in enumerate(sound[::100]):
